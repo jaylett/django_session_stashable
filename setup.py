d6e02ea75d51b058f08a809be37661619156fa17
Hidde-Jan Jongsma <me@hidde-jan.nl>
2011-07-15 11:19:27 +0200

    Made repo a python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django Session Stashable',
+      version='1.0',
+      description='',
+      author='James Aylett',
+      url='https://github.com/jaylett/django_session_stashable',
+      packages=['django_session_stashable']
+    )
